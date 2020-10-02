511bd1453b1abab8bcb2a4d76af627a7ddf93183
oberest <oleg.berest@NetCracker.com>
2020-10-02 10:10:53 +0300

    Add new functional

--- src/com/test/project/Main.java
+++ src/com/test/project/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        find("A");
+    }
+
+    private static void find(String a) {
+        System.out.println("OK");
     }
 }
